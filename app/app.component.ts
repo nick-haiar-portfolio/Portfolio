97b88a7bd333800c1e642570efa5d0e5b4706e80
Nick Haiar <nicholashaiar@gmail.com>
2016-10-08 17:50:16 -0400

    working app

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    template: '<h1>Root Component</h1>'
+})
+
+export class RootComponent { }
---
+++ app/app.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { RootComponent } from './app.component';
+
+@NgModule({
+    imports: [ BrowserModule ],
+    declarations: [ RootComponent ],
+    bootstrap: [ RootComponent ]
+})
+
+export class AppModule { }
